e1c042d057eb3a09aae2779150611145c345d8a4
matiasg1200 <88252942+matiasg1200@users.noreply.github.com>
2022-02-10 12:43:16 -0300

    Update storage.tf

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "tf-bucket-874969" { 
-    name = "tf-bucket-874969" 
+resource "google_storage_bucket" "" { 
+    name = "" 
     location = "US" 
     force_destroy = true 
     uniform_bucket_level_access = true 
