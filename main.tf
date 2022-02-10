7d97bbe1cf0241368380922714a6556e5059a90b
matiasg1200 <88252942+matiasg1200@users.noreply.github.com>
2022-02-10 12:33:45 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
