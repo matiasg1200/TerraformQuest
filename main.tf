48f47b9d388f1a05a3d642c9c78ed4a7c469cc35
matiasg1200 <88252942+matiasg1200@users.noreply.github.com>
2022-02-10 12:45:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 terraform {
+  #task3
+  #backend "gcs" { 
+  #  bucket = "tf-bucket-874969" 
+  #  prefix = "terraform/state" 
+  #  }  
   required_providers {
     google = {
       source = "hashicorp/google"
@@ -6,8 +11,48 @@ terraform {
   }
 }
 provider "google" {
-  version = "3.5.0"
-  project = "<PROJECT_ID>"
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
+
+module "instances" {
+    source = "./modules/instances"
+}
+
+module "storage" {
+    source = "./modules/storage"
+}
+
+#Task6
+#module "network" {
+#  source  = "terraform-google-modules/network/google"
+#  version = "3.4.0"
+  # insert the 3 required variables here
+#  project_id   = var.project_id
+#  network_name = ""
+#  routing_mode = "GLOBAL"
+#  subnets = [
+#    {
+#        subnet_name           = "subnet-01"
+#        subnet_ip             = "10.10.10.0/24"
+#        subnet_region         = "us-central1"
+#    },
+#    {
+#        subnet_name           = "subnet-02"
+#        subnet_ip             = "10.10.20.0/24"
+#        subnet_region         = "us-central1"
+#    }
+#  ]
+#}
+
+#Task7
+#resource "google_compute_firewall" "tf-firewall" {
+#  name    = "tf-firewall"
+#  network = "projects/<PROJECT_ID>/global/networks/<NETWORK_NAME>"
+#  allow {
+#    protocol = "tcp"
+#    ports    = ["80"]
+#  }
+#  source_ranges = ["0.0.0.0/0"] 
+#}
