472ce23201f1de1f1890be1260b31b364dced82f
matiasg1200 <88252942+matiasg1200@users.noreply.github.com>
2022-02-10 12:34:11 -0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "us-central1"
+}
+
+variable "zone" {
+    default = "us-central1-a"
+}
+
+variable "project_id" {
+    default = "qwiklabs-gcp-01-d721ae76744f"
+}
