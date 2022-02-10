231c1e1cf731208baf71af40c769961421e82127
matiasg1200 <88252942+matiasg1200@users.noreply.github.com>
2022-02-10 16:49:17 -0300

    Update instances.tf

--- modules/instances/instances.tf
+++ modules/instances/instances.tf
@@ -1,34 +1,24 @@
-resource "google_compute_instance" "tf-instance-1" {
-  project      = var.project_id
-  name         = "tf-instance-1"
-  machine_type = "n1-standard-1"
-  zone         = var.zone
-  metadata_startup_script = <<-EOT
-        #!/bin/bash
-    EOT
-  allow_stopping_for_update = true
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
+resource "google_compute_instance" "tf-instance-1" { 
+    name         = "tf-instance-1" 
+    machine_type = "n1-standard-2" 
+    zone         = var.zone
+    allow_stopping_for_update = true
+    boot_disk { 
+      initialize_params { 
+        image = "debian-cloud/debian-10" 
+      } 
     }
-  }
-  network_interface {
-    network = "default"
-    #Task6
-    #subnetwork = "subnet-01"
-    access_config {
-    }
-  }
+    network_interface { 
+      network    = "default"
+      #Task6
+      #subnetwork = "subnet-01" 
+    } 
 }
 
 resource "google_compute_instance" "tf-instance-2" {
-  project      = var.project_id
   name         = "tf-instance-2"
   machine_type = "n1-standard-1"
   zone         = var.zone
-  metadata_startup_script = <<-EOT
-        #!/bin/bash
-    EOT
   allow_stopping_for_update = true
   boot_disk {
     initialize_params {
@@ -39,8 +29,6 @@ resource "google_compute_instance" "tf-instance-2" {
     network = "default"
     #Task6
     #subnetwork = "subnet-02"
-    access_config {
-    }
   }
 }
 
